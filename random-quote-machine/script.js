96dd6ffdc56652fcb76296c0cee8e0b5f8735da8
carlcorn <caalojisan@gmail.com>
2019-02-22 03:07:57 +0900

    version 1.0

--- random-quote-machine/script.js
+++ random-quote-machine/script.js
@@ -4,7 +4,7 @@ let curQuote = {quote: "", author: ""};
 // =====================global functions=======================
 
 function pickColor() {
-  let colors = ['#2e93b1', '#dda9b4', '#dab420', '#941414', '#3eb1c8', '#ffcc5c', '#a31616', '#001e3b', '#00cc66', '#ffb399', '#ffbd66'];
+  let colors = ['#2fed71', '#2e93b1', '#dda9b4', '#dab420', '#941414', '#3eb1c8', '#ffcc5c', '#a31616', '#001e3b', '#00cc66', '#ffb399', '#ffbd66'];
   let index = Math.floor(Math.random() * Math.floor(colors.length));
   return colors[index];
 };
@@ -13,14 +13,6 @@ function getQuotes() {
   fetch('https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json')
   .then((response) => response.json())
   .then((json) => {
-    // let thisQuote = json.quotes[0].quote;
-    // let output = `<h1>${thisQuote}</h1>`
-    // document.getElementById('text').innerHTML = output;
-    // let thisQuote = {};
-    // Object.assign(thisQuote, curQuote);
-    // while(curQuote === thisQuote){
-    //   Object.assign(curQuote, pickQuote(json.quotes));
-    // }
     let newQuote = pickQuote(json.quotes);
     // console.log("curQuote: " + curQuote.quote);
     while(newQuote.quote === curQuote.quote){
@@ -28,8 +20,8 @@ function getQuotes() {
     }
     // console.log(newQuote);
     if(curQuote.quote == ""){
-      document.getElementById('text').innerHTML = `<h1>${newQuote.quote}</h1>`;
-      document.getElementById('author').innerHTML = `<p>${newQuote.author}</p>`;
+      document.getElementById('text').innerHTML = `<span>${newQuote.quote}</span>`;
+      document.getElementById('author').innerHTML = `<p>- ${newQuote.author}</p>`;
     }else{
       renderQuote(newQuote);
     }
@@ -48,16 +40,20 @@ function pickQuote(quotes) {
 function renderQuote(newQuote) {
   // console.log(newQuote);
   $("#text").fadeOut(500, () => {
-    document.getElementById('text').innerHTML = `<h1>${newQuote.quote}</h1>`;
+    document.getElementById('text').innerHTML = `<span>${newQuote.quote}</span>`;
     $("#text").css("color", curColor);
   });
   $("#author").fadeOut(500, () => {
-    document.getElementById('author').innerHTML = `<p>${newQuote.author}</p>`;
+    document.getElementById('author').innerHTML = `<p>- ${newQuote.author}</p>`;
     $("#author").css("color", curColor);
+  });
+  $(".quotemark").fadeOut(500, () => {
+    $(".quotemark").css("color", curColor);
   });
 
   $("#text").fadeIn(500);
   $("#author").fadeIn(500);
+  $(".quotemark").fadeIn(500);
 }
 
 // =======================Main=================================
@@ -81,4 +77,7 @@ $(document).ready(() => {
 
   });
 
+  $("#tweet-quote").click(() => {
+    window.open(`https://twitter.com/intent/tweet?text=\"${curQuote.quote}\" - ${curQuote.author}`);
+  });
 });

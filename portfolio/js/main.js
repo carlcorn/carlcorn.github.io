1939d32a5e692629e927044f61db98f56c8541dd
carlcorn <caalojisan@gmail.com>
2019-03-13 15:09:13 +0900

    Added aboutme page

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,28 +1,40 @@
 const menu = document.querySelector('.menu');
+const body = document.querySelector('body');
 const menuBtn = document.querySelector('.menu-btn');
 const backBtn = document.querySelector('.back-btn');
 const menuNav = document.querySelector('.menu-nav');
 const navItem = document.querySelectorAll('.nav-item');
+const currPage = document.querySelector('.current')
 
 //Inital state of menu
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
+currPage.addEventListener('click', closeMenu);
+
 
 function toggleMenu() {
     if(!showMenu) {
-        menuBtn.classList.add('close');
-        backBtn.classList.add('show');
-        menu.classList.add('show');
-        menuNav.classList.add('show');
-        navItem.forEach(item => item.classList.add('show'));
-        showMenu = true;
+        openMenu();
     } else {
-        menuBtn.classList.remove('close');
-        backBtn.classList.remove('show');
-        menu.classList.remove('show');
-        menuNav.classList.remove('show');
-        navItem.forEach(item => item.classList.remove('show'));
-        showMenu = false;
+        closeMenu();
     }
+}
+
+function openMenu() {
+    menuBtn.classList.add('close');
+    backBtn.classList.add('show');
+    menu.classList.add('show');
+    menuNav.classList.add('show');
+    navItem.forEach(item => item.classList.add('show'));
+    showMenu = true;
+}
+
+function closeMenu() {
+    menuBtn.classList.remove('close');
+    backBtn.classList.remove('show');
+    menu.classList.remove('show');
+    menuNav.classList.remove('show');
+    navItem.forEach(item => item.classList.remove('show'));
+    showMenu = false;  
 }
